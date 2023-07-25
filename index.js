9c55bd419b7f236a846edac6d7021170c37e42bf
Mehla <mehlag@student.douglascollege.ca>
2023-07-25 14:13:59 -0700

    done

--- index.js
+++ index.js
@@ -7,15 +7,14 @@ const app = express();
 const port = 7000;
 
 // MongoDB Connection URL
-const mongoURI = 'mongodb://localhost:27017/Exam';
-// const mongoURI = 'mongodb+srv://root:VRPd3ICLK6juCQCa@cluster0.dg4ocjz.mongodb.net/Exam?retryWrites=true&w=majority';
+// const mongoURI = 'mongodb://localhost:27017/Exam';
+ const mongoURI = 'mongodb+srv://root:VRPd3ICLK6juCQCa@cluster0.dg4ocjz.mongodb.net/Exam?retryWrites=true&w=majority';
 // const mongoURI = 'mongodb+srv://tempuser:123@cluster0.f9d6o.gcp.mongodb.net/Exam';
 
 // Connect to MongoDB
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
-    console.log('Inserting to MongoDB');
     insertdOC();
   })
   .catch(err => {
@@ -25,11 +24,15 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
 // Middleware to parse incoming JSON data
 app.use(bodyParser.json());
 
-// Root route for GET request
-app.get('/', (req, res) => {
-    insertdOC();
+app.get('/insert', async (req, res) => {
+    let result = await insertdOC();
 
-    res.send('data is inserted successfully!');
+    res.json({result});
+});
+
+// Root route for GET request
+app.get('/', async (req, res) => {
+    res.send("Service is running. hit /insert to insert the data in the db (" + mongoURI + ")");
 });
 
 // Start the server
@@ -38,16 +41,22 @@ app.listen(port, () => {
 });
 
 function insertdOC() {
-    // Data to be inserted
-    const name = 'Gaurav Mehla';
-    const sid = '300347326';
+    console.log('Inserting to MongoDB');
+    return new Promise((resolve, reject) => {
+        // Data to be inserted
+        const name = 'Gaurav Mehla';
+        const sid = '300347326';
 
-    // Insert data into the "quizes" collection using the function
-    insertQuizData(name, sid)
-      .then(result => {
-        console.log('Document inserted successfully:', result);
-      })
-      .catch(err => {
-        console.error('Error inserting data:', err);
-      });
+        // Insert data into the "quizes" collection using the function
+        insertQuizData(name, sid)
+        .then(result => {
+            console.log('Document inserted successfully:', result);
+            resolve(result);
+        })
+        .catch(err => {
+            console.error('Error inserting data:', err);
+            reject(err);
+        });
+    });
+    
 }
