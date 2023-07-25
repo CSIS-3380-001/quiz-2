ceab0b7f7b50c7a9dc99007609da008b94820b6c
Mehla <mehlag@student.douglascollege.ca>
2023-07-25 13:59:01 -0700

    get route updated

--- index.js
+++ index.js
@@ -7,15 +7,37 @@ const app = express();
 const port = 7000;
 
 // MongoDB Connection URL
-// const mongoURI = 'mongodb://localhost:27017/Exam';
+const mongoURI = 'mongodb://localhost:27017/Exam';
 // const mongoURI = 'mongodb+srv://root:VRPd3ICLK6juCQCa@cluster0.dg4ocjz.mongodb.net/Exam?retryWrites=true&w=majority';
-const mongoURI = 'mongodb+srv://tempuser:123@cluster0.f9d6o.gcp.mongodb.net/Exam';
+// const mongoURI = 'mongodb+srv://tempuser:123@cluster0.f9d6o.gcp.mongodb.net/Exam';
 
 // Connect to MongoDB
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
-    
+    console.log('Inserting to MongoDB');
+    insertdOC();
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+// Middleware to parse incoming JSON data
+app.use(bodyParser.json());
+
+// Root route for GET request
+app.get('/', (req, res) => {
+    insertdOC();
+
+    res.send('data is inserted successfully!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}/`);
+});
+
+function insertdOC() {
     // Data to be inserted
     const name = 'Gaurav Mehla';
     const sid = '300347326';
@@ -28,20 +50,4 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
       .catch(err => {
         console.error('Error inserting data:', err);
       });
-  })
-  .catch(err => {
-    console.error('Error connecting to MongoDB:', err);
-  });
-
-// Middleware to parse incoming JSON data
-app.use(bodyParser.json());
-
-// Root route for GET request
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}/`);
-});
+}
